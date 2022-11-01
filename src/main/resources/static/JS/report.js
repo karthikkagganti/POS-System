39090a26e03b62331d936054740346218fa3a078
karthikkagganti <karthikkagganti@gmail.com>
2022-11-01 21:53:21 +0530

    working on generating unique reports

--- src/main/resources/static/JS/report.js
+++ src/main/resources/static/JS/report.js
@@ -1,30 +1,50 @@
 const list = document.getElementById("report-list");
 const button = document.getElementById("generate-report");
 const content = document.getElementById("report-content");
-fetchContent()
-button.addEventListener("click", function(event){
-    event.preventDefault();
-    generateReports();
-})
+var reports = [];
+var flag = 0;
 
-async function generateReports(){
-    let response = await fetch("/report",{
-        method:"POST",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-        }
-    })
-    let reportid = await (response.json())
-    fetchReport(reportid)
-    
+const today = new Date();
+const dd = String(today.getDate()).padStart(2, "0");
+const mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+const yyyy = today.getFullYear();
+today = yyyy + "-" + mm + "-" + dd;
+
+fetchContent();
+button.addEventListener("click", function (event) {
+  event.preventDefault();
+  generateReports();
+});
+
+function checkFlag() {
+  reports.forEach((ele) => {
+    if (ele.reportDate === today) {
+      flag = 1;
+    } else {
+      flag = 0;
+    }
+  });
+}
+async function generateReports() {
+  checkFlag();
+  if (flag == 0) {
+    let response = await fetch("/report", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+    let reportid = await response.json();
+    fetchReport(reportid);
+  }
 }
 
-function displayReports(array){
-    list.textContent=''
-    array.forEach(element => {
-       let li = document.createElement("li");
-       li.innerHTML=`
+function displayReports(array) {
+  list.textContent = "";
+  array.forEach((element) => {
+    let li = document.createElement("li");
+    li.innerHTML = `
        <div class="column">
             <div class="card" style="width:250px;height:250px">
                 <div class="card-content">
@@ -39,51 +59,51 @@ function displayReports(array){
                 </div>
             </div>
         </div>
-       `
-       list.appendChild(li)
-    }); 
+       `;
+    list.appendChild(li);
+  });
 }
 
-function deleteReport(id){
-    fetch(`/report/${id}`,{
-        method:"DELETE",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-        }
-    })
-    .then(response => console.log(response))
+function deleteReport(id) {
+  fetch(`/report/${id}`, {
+    method: "DELETE",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  }).then((response) => console.log(response));
 
-    location.reload();
+  location.reload();
 }
-async function fetchContent(){
-    let  response = await fetch(`/report`)
-    array = await response.json();
-    displayReports(array);
+async function fetchContent() {
+  let response = await fetch(`/report`);
+  array = await response.json();
+  reports = array;
+  displayReports(array);
 }
-async function fetchReport(id){
-    let  response = await fetch(`/report/${id}`)
-    let array = await response.json();
-    fetchContent();
-    return array;
+async function fetchReport(id) {
+  let response = await fetch(`/report/${id}`);
+  let array = await response.json();
+  fetchContent();
+  return array;
 }
 
-async function temp(id){
-    console.log(id)
-    let array = await viewReport(id)
-    console.log(array)
-    let ordered_list = document.getElementById("order-list")
-    let expenses_list = document.getElementById("expense-list")
-    let reportDate = document.getElementById("reportDate");
-    let report_subtitle = document.getElementById("report-subtitle");
-    let sales = document.getElementById("sales");
-    report_subtitle.innerHTML=`total amount after expenses: ₹${array.total}`
-    sales.innerHTML=`Number Of Orders: ${array.noSales} `
-    reportDate.innerHTML=`${array.reportDate}`
-    ordered_list.innerHTML=``;
-    array.orderedItems.forEach((element)=>{
-        let li = document.createElement("li");
-        li.innerHTML =`
+async function temp(id) {
+  console.log(id);
+  let array = await viewReport(id);
+  console.log(array);
+  let ordered_list = document.getElementById("order-list");
+  let expenses_list = document.getElementById("expense-list");
+  let reportDate = document.getElementById("reportDate");
+  let report_subtitle = document.getElementById("report-subtitle");
+  let sales = document.getElementById("sales");
+  report_subtitle.innerHTML = `total amount after expenses: ₹${array.total}`;
+  sales.innerHTML = `Number Of Orders: ${array.noSales} `;
+  reportDate.innerHTML = `${array.reportDate}`;
+  ordered_list.innerHTML = ``;
+  array.orderedItems.forEach((element) => {
+    let li = document.createElement("li");
+    li.innerHTML = `
         <div class="cards">
         <div class="card" style="width: 200px; height: 200px">
           <div class="card-content">
@@ -95,13 +115,13 @@ async function temp(id){
           </div>
         </div>
         </div>
-        `
-        ordered_list.appendChild(li)
-    })
-    expenses_list.innerHTML=``;
-    array.expenses.forEach((element)=>{
-        let li = document.createElement("li");
-        li.innerHTML =`
+        `;
+    ordered_list.appendChild(li);
+  });
+  expenses_list.innerHTML = ``;
+  array.expenses.forEach((element) => {
+    let li = document.createElement("li");
+    li.innerHTML = `
         <div class="column">
           <div class="card" style="width: 200px; height: 100px">
             <div class="card-content">
@@ -112,24 +132,20 @@ async function temp(id){
             </div>
           </div>
         </div>
-        `
-        expenses_list.appendChild(li)
-    })
+        `;
+    expenses_list.appendChild(li);
+  });
 
-
-    let area = document.getElementById("view-area")
-    area.style.display = "block";
-
-    
+  let area = document.getElementById("view-area");
+  area.style.display = "block";
 }
 
-async function viewReport(id){
-    let  response = await fetch(`/report/${id}`)
-    let array = await response.json();
-    return array;
-
+async function viewReport(id) {
+  let response = await fetch(`/report/${id}`);
+  let array = await response.json();
+  return array;
 }
-function closeWindow(){
-    console.log("here")
-    document.getElementById("view-area").style.display="none";
+function closeWindow() {
+  console.log("here");
+  document.getElementById("view-area").style.display = "none";
 }
